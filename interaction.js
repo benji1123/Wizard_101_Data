ba57010aa791e34682afc79e514cf517490822c6
Ben Li <benji112358@gmail.com>
2018-06-19 22:41:41 -0400

    basic frontend

---
+++ interaction.js
@@ -0,0 +1,59 @@
+
+//CONTAINER for g.u.i. 
+let container = document.getElementById('container');	
+
+//DATA..................................... .................................................................................................
+let attacks=["ATTACKS","Thunder Snake","Lightning Bats","Storm Shark","Kraken","Triton","Stormzilla", "Storm Lord","Tempest","Sirens"];
+let shields=["SHEILDS","Storm","Thermic","Dream"];
+let blades=["BLADES","Storm","Elemental"]; 
+let traps = ["TRAPS","Storm","Feint"];
+let deck = [blades, attacks, traps, shields];	// modularize deck
+
+let HISTORY = [];				// TEMP STORAGE 			
+
+
+//BUTTONS....................................................................................................................................
+for(k=0; k<deck.length; k++) {
+
+	let subdeck = deck[k];	// where SUBDECKS = blades, attacks, etc.
+	// set TITLE of subdecks accordingly
+	let title = document.createElement('h1');						
+	title.textContent = subdeck[0];	title.classList.add('title'); //CSS	
+	// CONTAINER for buttons
+	let col = document.createElement('div');
+	col.classList.add("col");
+
+	// Populate Subdeck-Container with CARDS	
+	for(r=1; r<subdeck.length; r++) { 
+		let card = document.createElement('button');		
+		let name_card = subdeck[r];	
+		card.textContent = name_card;						// label cards
+
+		card.addEventListener('click', () => {			
+			HISTORY.push(name_card);						// add card to history 
+			console.log(HISTORY);
+		});
+		col.appendChild(card)								
+	}
+	container.append(title,col);									
+}
+
+
+// DATA TRANSFER....................................................................................................................................
+
+// MODULARIZE DATA  
+let pusher = document.createElement('button');
+pusher.textContent = "PUSH";
+
+let DISPLAY = document.createElement('ol');		// store session data
+
+// PUSH BUTTON
+pusher.addEventListener('click', () => {
+	// 
+	let temp = document.createElement('li');
+	temp.textContent = HISTORY;
+	DISPLAY.appendChild(temp);
+	HISTORY = [];					// reset data variable
+});
+
+container.append(pusher, DISPLAY);
---
+++ readme.md
@@ -0,0 +1,17 @@
+Here's the Plan, Ben...
+
+1) create the HTML layout for the app
+-fully text based (no pictures)
+-create a button-set for each group (attack cards, blades, shields)
+
+
+2) allow the app to retain what you press JS 
+-allow users to temporarily store data before pushing it to the database
+-allow users to edit temporarily stored data 
+
+
+3) style the app
+
+
+4) make use of the data
+-create visualizations of the data (somehow)
